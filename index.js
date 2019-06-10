f193fdff1afd4e2dd8427346db2fad8260e84268
gc6123 <ilovegrace09@gmail.com>
2019-06-10 17:26:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,41 @@ function currentLine(line) {
     return ("The line is currently empty.")
   }
 }
+
+function takeANumber(katzDeliLine, personName) {
+  katzDeliLine.push(personName);
+  return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  var firstInLine = katzDeliLine[0]
+  if (katzDeliLine.length>0) {
+    katzDeliLine.shift()
+    return (`Currently serving ${firstInLine}`);
+  }
+  else {
+    return("There is nobody waiting to be served!")
+  }
+}
+
+function currentLine(line) {
+  if (line.length>0) {
+  var n=1;
+  for (let i=0; i<line.length; i++); {
+    line[i]=`${n}.${line[i]}`;
+  n++
+    }
+  return(`The line is currently: ${line}`)
+}
+else {
+  return ("The line is currently empty");
+}
+}
+
+
+
+
+
+
+
+
